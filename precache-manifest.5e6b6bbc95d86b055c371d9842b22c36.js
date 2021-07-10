5d7dca5c30e88da33f1e0e605ff252c46b075019
sarahbahi <sarahbahyy@gmail.com>
2021-07-10 19:16:43 +0200

    Updates

---
+++ precache-manifest.5e6b6bbc95d86b055c371d9842b22c36.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "db02a9d27f474a6f0396",
+    "url": "/react-library-app/static/css/main.fe69c4a3.chunk.css"
+  },
+  {
+    "revision": "db02a9d27f474a6f0396",
+    "url": "/react-library-app/static/js/main.db02a9d2.chunk.js"
+  },
+  {
+    "revision": "a9b7a35fcebaaf69b424",
+    "url": "/react-library-app/static/js/1.a9b7a35f.chunk.js"
+  },
+  {
+    "revision": "69f987d41ec57e23fee4",
+    "url": "/react-library-app/static/js/runtime~main.69f987d4.js"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/react-library-app/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/react-library-app/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/react-library-app/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "5fabc38827dd8c8cf575782ebc146dcd",
+    "url": "/react-library-app/index.html"
+  }
+];
